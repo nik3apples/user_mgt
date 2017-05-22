0c8830014bf2f02bf2f9e04af4ff8f42f159590f
Nikhil <nikhil@3apples.co>
2017-05-22 14:05:55 +0530

    added return for errs

--- controllers/update.js
+++ controllers/update.js
@@ -17,4 +17,20 @@ controller.update_email = function (req, res, next) {
 	});
 };
 
+controller.update_password = function (req, res, next) {
+	var data = req.body;
+	var user_info = {
+		user_id       : data.user_id,
+		old_password  : data.old_password,
+		new_password  : data.new_password	
+	};
+
+	model.update_email (user_info, function (err, result) {
+		if (err)
+			return res.send (err);
+
+		return res.send (result);
+	});
+};
+
 module.exports = controller;
--- routes/update.js
+++ routes/update.js
@@ -4,5 +4,6 @@ var router     = express.Router();
 var controller = require('../controllers/update');
 
 router.post ('/email', controller.update_email);
+router.post ('/password', controller.update_password);
 
 module.exports = router;
